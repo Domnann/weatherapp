dfcfa9f928db51e2df5142ce4f69c63dc03f2819
Domnann <domnangambo@gmail.com>
2022-07-05 21:46:24 +0100

    updated api calls

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -17,14 +17,14 @@ def index(request):
                 "city_name": str(list_of_data['name']),
                 "country_code": str(list_of_data['sys']['country']),
                 "coordinate": str(list_of_data['coord']['lon'])+  ',' + str(list_of_data['coord']['lat']),
-                "temp": str(list_of_data['main']['temp'])+ "C",
-                "pressure": str(list_of_data['main']['pressure']),
-                "humidity": str(list_of_data['main']['humidity']),
+                "temp": str(list_of_data['main']['temp'])+ "°C",
+                "pressure": str(list_of_data['main']['pressure'])+ "hPa",
+                "humidity": str(list_of_data['main']['humidity']) + "%",
                 "main": str(list_of_data['weather'][0]['main']),
                 "description": str(list_of_data['weather'][0]['description']),
                 "icon": str(list_of_data['weather'][0]['icon']),
-                "temp_min": str(list_of_data['main']['temp_min']),
-                "temp_max": str(list_of_data['main']['temp_max']),
+                "temp_min": str(list_of_data['main']['temp_min'])+ "°C",
+                "temp_max": str(list_of_data['main']['temp_max'])+ "°C",
             }
 
             # print(data)
